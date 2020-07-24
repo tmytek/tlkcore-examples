0362ca4fa5a41f34528cdebfc3dfcf7d72e54928
tobias_huang <tobias_huang@tmytek.com>
2020-07-24 10:56:41 +0800

    [update] update example and doc for v3.0.4 at VC++

--- example/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ example/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,15 +12,10 @@ using namespace System;
 using namespace BBoxAPI;
 using namespace msclr::interop;
 
-enum class TRMODE
-{
-	TX = 0,
-	RX = 1,
-};
 
 int main()
 {
-	BBoxOneAPI ^b = gcnew BBoxOneAPI();
+	BBoxAPI::BBoxOneAPI ^b = gcnew BBoxAPI::BBoxOneAPI();
 
 	//array<String ^>^ strarray = b->getStringArray();
 
@@ -42,60 +37,45 @@ int main()
 
 	array<String ^>^ dev_info = b->ScanningDevice();
 
-	for (int i = 0; i < dev_info->Length; i++)
-		Console::WriteLine("device info from API side : " + dev_info[i]);
+	// suppose only one bboxone device
+	array<String^>^ info_arr = dev_info[0]->Split(',');
 
+	for (int i = 0; i < info_arr->Length; i++)
+		Console::WriteLine(info_arr[i]);
 
-	/* It will send the init command to BBox. */
-	/* first BBoxOne */
-	String^  s_info_1 = b->Init("B19312200-24", 0);
-
-	Console::WriteLine(s_info_1);
-	Console::WriteLine("Init first one");
-
-	int dev_1_spacing = b->selectAntenna(Device::AntennaType::FOURBYFOUR, 0);
-	Console::WriteLine("1st device antenna spacing : " + dev_1_spacing);
-	b->SwitchTxRxMode((int)TRMODE::TX, 0);
-
-
-#if MULTIPLEBBOXONE
-	/* second BBoxOne */
-	String^  s_info_2 = b->Init("B19312300-24", 1);
-
-	Console::WriteLine(s_info_2);
-	Console::WriteLine("Init second one");
-
-	int dev_2_spacing = b->selectAntenna(Device::AntennaType::FOURBYFOUR, 0);
-	Console::WriteLine("2nd device antenna spacing : " + dev_2_spacing);
-	b->SwitchTxRxMode((int)TRMODE::TX, 1);
 	
-#endif 
+	
+
+	/* It will send the init command to BBoxOne. */
+	String^ sn = info_arr[0]; // sn
+	String^ s_info = b->Init(sn, 0/*BBoxOne*/, 0);
+
+	Console::WriteLine(s_info);
+
+	b->selectAAKit("TMYTEK_4x4", sn);
+	
+	b->SwitchTxRxMode(0/*Tx*/, sn);
+
+	b->getTxRxMode(sn);
+
+
+
+
 	Console::WriteLine("press enter to continue....");
 	Console::Read();
 
-	int deg = 0;
-	int dir = -1;
-	for (int count = 0; count < 1000; count++)
+	int deg = 10;
+	
+	for (int count = 0; count < 1; count++)
 	{
 		try
 		{
-			s_info_1 = b->setBeamX(5/*dB*/, deg/*degree*/, 0);
-			s_info_1 = b->setBeamY(5/*dB*/, deg/*degree*/, 0);
-			s_info_1 = b->setBeamXY(5/*dB*/, deg/*degree*/, deg/*degree*/, 0);
-			Console::WriteLine("Firsr Device control : " + s_info_1);
-#if MULTIPLEBBOXONE
-
-			s_info_2 = b.setBeamX(0, deg, 1);
-			s_info_2 = b.setBeamY(0, deg, 1);
-			s_info_2 = b.setBeamXY(0, deg, deg, 1);
-			Console::WriteLine("Second Device control : " + s_info_2);
-#endif
-
-			if (deg <= -25)
-				dir = 1;
-			else if (deg >= 25)
-				dir = -1;
-			deg += dir;
+			s_info = b->setBeamX(0/*dB*/, deg/*degree*/, sn);
+			Console::WriteLine(s_info);
+			s_info = b->setBeamY(0/*dB*/, deg/*degree*/, sn);
+			Console::WriteLine(s_info);
+			s_info = b->setBeamXY(0/*dB*/, deg/*degree*/, deg/*degree*/, sn);
+			Console::WriteLine(s_info);
 		}
 		catch (...)
 		{
